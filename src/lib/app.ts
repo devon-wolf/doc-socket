4a5d480dd69134d38b983821f31b515ebb6c9586
Devon Wolfkiel <devonwolf@tutanota.com>
2021-06-05 12:04:18 -0700

    Complete basic tutorial

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,12 +1,11 @@
 import express = require('express');
-const cors = require('cors');
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
 
-app.use(require('./middleware/not-found'));
-app.use(require('./middleware/error'));
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+})
 
 module.exports = app;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,18 @@
-const app = require('./lib/app');
+import app = require('./lib/app');
+import http = require('http');
+import { Server } from 'socket.io';
+
+const server = http.createServer(app);
+const io = new Server(server);
 
 const PORT = process.env.PORT || 7890;
 
-app.listen(PORT, () => {
+io.on('connection', socket => {
+	socket.on('chat message', msg => {
+		io.emit('chat message', msg);
+	});
+});
+
+server.listen(PORT, () => {
 	console.log(`Started on ${PORT}`);
 });
