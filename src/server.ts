8c83f1dfe439c68cd16753b047ec63d44d8d6090
Devon Wolfkiel <devonwolf@tutanota.com>
2021-06-19 11:32:25 -0700

    Add environment variable for frontend url

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { Server } from 'socket.io';
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-	  origin: "http://localhost:3000",
+	  origin: process.env.FRONTEND_URL,
 	  methods: ["GET", "POST"],
 	  allowedHeaders: ["document-events"],
 	  credentials: true
